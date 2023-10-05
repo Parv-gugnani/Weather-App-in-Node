f1953a561392ab62add83fd675a75f907002bad2
Parv-gugnani <9765.parv@gmail.com>
2023-10-05 17:06:02 +0530

    async

---
+++ app.js
@@ -0,0 +1,10 @@
+//asynchronous video
+console.log("Starting");
+
+//this will stop the time
+setTimeout(() => {}, 2000);
+setTimeout(() => {
+  console.log("0 sec code");
+}, 0);
+// , this is time milliseconds we added 2 sec timeout
+console.log("stoping");
