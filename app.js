5bf96e3be38717a7ab17e0590f644b7e5ae7d404
Parv-gugnani <9765.parv@gmail.com>
2023-10-07 15:13:12 +0530

    env

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ setTimeout(() => {
 // , this is time milliseconds we added 2 sec timeout
 console.log("stoping");
 */
+const request = require("request");
+const myApi = process.env.myApi;
+
+//Url
+const url = `https://api.weatherstack.com/current?access_key=${myApi}&query=New%20York`;
+
+request({ url: url }, (error, response) => {
+  // console.log(response);
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
